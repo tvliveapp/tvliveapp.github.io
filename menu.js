6b7dc569da1f50c768fe4a1c76ae0b50dcbd4500
tvliveapp <61073350+tvliveapp@users.noreply.github.com>
2024-12-29 17:21:17 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ var elements = document.getElementsByClassName("arrow-togglable");
 var currentIndex = 0;
 console.log(elements)
 var menuIndex = 0;
-
+var 
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -13,11 +13,20 @@ function closeNav() {
 }
     
 
-
+function menu(){
+ console.log(menuIndex);
+  
+  
+}
 document.onkeydown = function(e) {
       console.log(e.keyCode,e.key) 
       document.getElementById("txt").innerHTML=e.key+" "+currentIndex;
       switch (e.keyCode) {
+        case 37:
+          menuIndex = (menuIndex == 3) ? 3 : ++menuIndex;
+          menu();
+          elements[currentIndex].focus();
+          break;
         case 38:
           currentIndex = (currentIndex == 0) ? elements.length - 1 : --currentIndex;
           elements[currentIndex].focus();
