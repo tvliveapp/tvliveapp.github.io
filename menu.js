13d1a2456537331290d745912c76a72ba6d03906
tvliveapp <61073350+tvliveapp@users.noreply.github.com>
2024-12-29 16:49:35 -0500

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,31 @@
+var elements = document.getElementsByClassName("arrow-togglable");
+var currentIndex = 0;
+console.log(elements)
+var menuIndex = 0;
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+    
+
+
+document.onkeydown = function(e) {
+      console.log(e.keyCode,e.key) 
+      document.getElementById("txt").innerHTML=e.key+" "+currentIndex;
+      switch (e.keyCode) {
+        case 38:
+          currentIndex = (currentIndex == 0) ? elements.length - 1 : --currentIndex;
+          elements[currentIndex].focus();
+          break;
+        case 40:
+          currentIndex = ((currentIndex + 1) == elements.length) ? 0 : ++currentIndex;
+          elements[currentIndex].focus();
+          break;
+      }
+    };
+
