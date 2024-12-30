cff9faaedb0ac574b748c89968e7584181d5ed4c
tvliveapp <61073350+tvliveapp@users.noreply.github.com>
2024-12-30 00:23:49 -0500

    Update streamtp2.js

--- streamtp2.js
+++ streamtp2.js
@@ -132,7 +132,7 @@ a=document.createElement("a");
 a.className= "arrow-togglable";
 a.innerHTML="streamtp2 TV";
 a.href="#";
-a.name="streamtp2"'
+a.name="streamtp2";
 mySidenav1.appendChild(a);
 
 for (let key in channels) {
