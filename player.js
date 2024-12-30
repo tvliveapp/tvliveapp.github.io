0c65028d8dda18903ac3bc5707d6acb03a4f76b0
tvliveapp <61073350+tvliveapp@users.noreply.github.com>
2024-12-29 20:44:59 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,4 +1,4 @@
-document.location="https://shawon9324.github.io/apps/keytester/"
+//document.location="https://shawon9324.github.io/apps/keytester/"
     var player="";
     var playbackURL = "https://cale.pricesaskeloadsc.com/eventos7/tracks-v1a1/mono.m3u8?token=811209c227c99f0c5b268d2b7b393e2e2072cb66-7c-1735553997-1735517997";
     var p2pConfig = {
