b1f073650382c10d608701c19a531fea865bcd97
tvliveapp <61073350+tvliveapp@users.noreply.github.com>
2024-12-29 22:24:11 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -3,17 +3,17 @@ var currentIndex = 0;
 console.log(elements)
 var menuIndex = 0;
 var menuList=[];
-
+var activeNAv="mySidenav1";
 function openNav(idx) {
   var slideName="mySidenav"+idx;
   if (idx && idx<3)
-    document.getElementById(slideName).style.width = "250px";
+    document.getElementById(idx).style.width = "250px";
 }
 
 function closeNav(idx) {
-  var slideName="mySidenav"+idx;
+  
   if (idx)
-    document.getElementById(slideName).style.width = "0";
+    document.getElementById(idx).style.width = "0";
 }
     
 
@@ -26,17 +26,19 @@ document.onkeydown = function(e) {
       document.getElementById("txt").innerHTML=e.key+" "+currentIndex;
       switch (e.keyCode) {
         case 39:
-          closeNav(menuIndex);
-          menuIndex = (menuIndex == 3) ? 3 : ++menuIndex;
-          openNav(menuIndex);
-          menu();
+          closeNav(document.activeElement.parentElement.id);
+          openNav(document.activeElement.parentElement.id+"C");
           elements[currentIndex].focus();
           break;
         case 37:
+          /*
           closeNav(menuIndex);
           menuIndex = (menuIndex == 0) ? 0 : --menuIndex;
           openNav(menuIndex);
           menu();
+          */
+          closeNav(document.activeElement.parentElement.id);
+          openNav(document.activeElement.parentElement.id.replace("C",""));
           elements[currentIndex].focus();
           break;
         case 38:
