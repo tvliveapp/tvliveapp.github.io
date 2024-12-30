d56a9ba77ac106c9adcbfa95e419424a2b6a9674
tvliveapp <61073350+tvliveapp@users.noreply.github.com>
2024-12-29 19:36:42 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 
     var player="";
-    var playbackURL = "https://vadp.pricesaskeloadsc.com:443/global/espn1/index.m3u8?token=e75232e6873dc8b1eb475a728352c51e8e5c8b78-67-1735286139-1735250139";
+    var playbackURL = "https://vasoc88.pricesaskeloadsc.com:443/global/eventos7/index.m3u8?token=811209c227c99f0c5b268d2b7b393e2e2072cb66-7c-1735553997-1735517997";
     var p2pConfig = {
         live: true,
         trackerZone: 'us',    
@@ -23,3 +23,4 @@
       player.load(streamUrl);
   }  
 
+play(playbackURL);
