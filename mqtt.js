3f7ccdcbfb26147942fb16481d8da2b7f393f110
tvliveapp <61073350+tvliveapp@users.noreply.github.com>
2026-02-18 14:39:35 -0500

    Update mqtt.js

--- mqtt.js
+++ mqtt.js
@@ -379,6 +379,7 @@ var websocketclient = {
 websocketclient.connect();
 
 function publicar(topico,data) {
+         data.id=websocketclient.clientId;
          websocketclient.publish(topico, data, 0, false);
         
   }
