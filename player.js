fa9e9b7e99d7bdbbaffc42a80aefc6a4198cd18a
tvliveapp <61073350+tvliveapp@users.noreply.github.com>
2026-02-17 19:50:23 -0500

    Update player.js

--- player.js
+++ player.js
@@ -1,6 +1,6 @@
 //document.location="https://shawon9324.github.io/apps/keytester/"
     var player="";
-    var playbackURL = "https://gzlet.envivotv.live/espn2/tracks-v1a1/mono.m3u8?remote=45.225.44.89&token=b9e94bf42401b11ecdca594d953b5dd3fbb8e130-fd458e331677ca88c4dfa6acfbb96e9a-1755655580-1755644780";
+    var playbackURL = "https://tyg2mnl9.fubohd.com/espn2/mono.m3u8?token=87bed6eaf0084f5791a74c933789dab87947f649-f0-1771392487-1771374487";
     var p2pConfig = {
         live: true,
         trackerZone: 'us',    
